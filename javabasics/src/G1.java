7840e980a4a0fcf4815940cbf27aaaa43f4eb6d0
sudhaneupane <sudaneupane.11@gmail.com>
2024-06-27 08:43:24 +0545

    some examples

--- javabasics/src/G1.java
+++ javabasics/src/G1.java
@@ -30,7 +30,7 @@ public class G1 extends Canvas {
 
 class ImageTest extends JFrame {
     ImageTest(G1 s) {
-        ImageIcon img = new ImageIcon("./360_F_210969565_cIHkcrIzRpWNZzq8eaQnYotG4pkHh0P9.jpg");
+        ImageIcon img = new ImageIcon("360_F_210969565_cIHkcrIzRpWNZzq8eaQnYotG4pkHh0P9.jpg");
         JLabel l = new JLabel();
         l.setIcon(img);
         setLayout(new FlowLayout());
